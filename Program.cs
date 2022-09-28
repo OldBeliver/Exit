33178fd0da47f6b13a6ee47dbc42e0773421d071
OldBeliver <380506338004@yandex.com>
2022-09-28 13:52:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "exit";
+            string userInput;
+            bool doRepeat = true;
+
+            while (doRepeat)
+            {
+                Console.WriteLine($"Введите команду для выхода:");
+                userInput = Console.ReadLine();
+
+                if(userInput == password)
+                {
+                    doRepeat = false;
+                }
+            }
+
+            Console.WriteLine($"Выход подтвержден");
+        }
+    }
+}
